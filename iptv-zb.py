c88d2542902e8791a4fdff24cebdb5eb2ab4c134
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-03-28 15:18:47 +0800

    Update iptv-zb.py

--- iptv-zb.py
+++ iptv-zb.py
@@ -4,15 +4,53 @@ import re
 import base64
 import datetime
 import requests
+import threading
+from queue import Queue
 from datetime import datetime
+
+#  远程获取港澳台直播源文件
+url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"
+r = requests.get(url)
+open('DIYP-v4.txt', 'wb').write(r.content)
+
+keywords = ['凤凰卫视', '凤凰资讯', 'TVB翡翠', 'TVB明珠', 'TVB星河', 'J2', '无线', '有线', '天映', 'VIU',
+            'RTHK', 'HOY', '香港卫视', 'Viu']  # 需要提取的关键字列表
+pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('HK.txt', 'w', encoding='utf-8') as HK:
+    HK.write('\n港澳频道,#genre#\n')
+    for line in file:
+        if re.search(pattern, line):  # 如果行中有任意关键字
+            HK.write(line)  # 将该行写入输出文件
+
+keywords = ['民视', '中视', '台视', '华视', '新闻台', '东森', '龙祥', '公视', '三立', '大爱', '年代新闻', '人间卫视',
+            '人間', '大立', '八大', 'TVBS', '視','中天','非凡', 'BBC']  # 需要提取的关键字列表
+pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('TW.txt', 'w', encoding='utf-8') as TW:
+    TW.write('\n台湾频道,#genre#\n')
+    for line in file:
+        if re.search(pattern, line):  # 如果行中有任意关键字
+            TW.write(line)  # 将该行写入输出文件
+
+# 读取要合并的香港频道和台湾频道文件
+file_contents = []
+file_paths = ["HK.txt", "TW.txt"]  # 替换为实际的文件路径列表
+for file_path in file_paths:
+    with open(file_path, 'r', encoding="utf-8") as file:
+        content = file.read()
+        file_contents.append(content)
+# 生成合并后的文件
+with open("GAT.txt", "w", encoding="utf-8") as output:
+    output.write('\n'.join(file_contents))
+
+# 线程安全的队列，用于存储下载任务
+task_queue = Queue()
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
 
-urls = ['changsha', 'zhuzhou', 'hengyang', 'yueyang', 'yiyang', 'huaihua', 'loudi',
-        'chenzhou', 'xiangtan', 'changde', 'yongzgou', 'shaoyang', 'zhangjiajie', 'xiangxi']  # 湖南省
-
-channels = [
+urls = ["changsha", "zhuzhou", "xiangtan", "hengyang", "shaoyang", "yueyang", "changde", "zhangjiajie", "yiyang",
+        "chenzhou", "yongzhou", "huaihua", "loudi"]
+channelsx = [
     "湖南卫视,http://8.8.8.8:8/udp/239.76.245.115:1234", "湖南经视,http://8.8.8.8:8/udp/239.76.245.116:1234",
     "湖南都市,http://8.8.8.8:8/udp/239.76.245.117:1234", "湖南电视剧,http://8.8.8.8:8/udp/239.76.245.118:1234",
     "湖南电影,http://8.8.8.8:8/udp/239.76.245.119:1234", "湖南娱乐,http://8.8.8.8:8/udp/239.76.245.121:1234",
@@ -129,16 +167,19 @@ channels = [
 ]
 
 results = []
+channel = []
 urls_all = []
+resultsx = []
 resultxs = []
+error_channels = []
 
 for url in urls:
     url_0 = str(base64.b64encode((f'"Server: udpxy" && city="{url}" && org="Chinanet"').encode("utf-8")), "utf-8")
     url_64 = f'https://fofa.info/result?qbase64={url_0}'
+    print(url_64)
     try:
         response = requests.get(url_64, headers=headers, timeout=15)
         page_content = response.text
-        print(f" {url}  访问成功")
         pattern = r'href="(http://\d+\.\d+\.\d+\.\d+:\d+)"'
         page_urls = re.findall(pattern, page_content)
         for urlx in page_urls:
@@ -150,53 +191,64 @@ for url in urls:
                 page_proctabl = re.findall(pattern, page_content)
                 if page_proctabl:
                     urls_all.append(urlx)
+                    print(f"{urlx} 可以访问")
+
             except requests.RequestException as e:
                 pass
     except:
-        print(f" {url}  访问失败")
+        print(f"{url_64} 访问失败")
         pass
 
 urls_all = set(urls_all)  # 去重得到唯一的URL列表
 for urlx in urls_all:
-    result = [f'{name},{url.replace("http://8.8.8.8:8", urlx)}' for name, url in
-              [line.strip().split(',') for line in channels]]
-    results.extend(result)
+    channel = [f'{name},{url.replace("http://8.8.8.8:8", urlx)}' for name, url in
+               [line.strip().split(',') for line in channelsx]]
+    results.extend(channel)
 
 results = sorted(results)
 
-#  远程获取港澳台直播源文件
-url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"
-r = requests.get(url)
-open('DIYP-v4.txt', 'wb').write(r.content)
 
-keywords = ['凤凰卫视', '凤凰资讯', 'TVB翡翠', 'TVB明珠', 'TVB星河', 'J2', '无线', '有线', '天映', 'VIU',
-            'RTHK', 'HOY', '香港卫视', 'Viu']  # 需要提取的关键字列表
-pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
-with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('HK.txt', 'w', encoding='utf-8') as HK:
-    HK.write('\n港澳频道,#genre#\n')
-    for line in file:
-        if re.search(pattern, line):  # 如果行中有任意关键字
-            HK.write(line)  # 将该行写入输出文件
+# 定义工作线程函数
+def worker():
+    while True:
+        result = task_queue.get()
+        channel_name, channel_url = result.split(',', 1)
+        try:
+            response = requests.get(channel_url, stream=True, timeout=3)
+            if response.status_code == 200:
+                result = channel_name, channel_url
+                resultsx.append(result)
+                numberx = (len(resultsx) + len(error_channels)) / len(results) * 100
+                print(
+                    f"可用频道：{len(resultsx)} , 不可用频道：{len(error_channels)} 个 , 总频道：{len(results)} 个 ,总进度：{numberx:.2f} %。")
+            else:
+                error_channels.append(result)
+                numberx = (len(resultsx) + len(error_channels)) / len(results) * 100
+                print(
+                    f"可用频道：{len(resultsx)} 个 , 不可用频道：{len(error_channels)} , 总频道：{len(results)} 个 ,总进度：{numberx:.2f} %。")
+        except:
+            error_channels.append(result)
+            numberx = (len(resultsx) + len(error_channels)) / len(results) * 100
+            print(
+                f"可用频道：{len(resultsx)} 个 , 不可用频道：{len(error_channels)} 个 , 总频道：{len(results)} 个 ,总进度：{numberx:.2f} %。")
 
-keywords = ['民视', '中视', '台视', '华视', '新闻台', '东森', '龙祥', '公视', '三立', '大爱', '年代新闻', '人间卫视',
-            '人間', '大立', '八大', 'TVBS', '視','中天','非凡', 'BBC']  # 需要提取的关键字列表
-pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
-with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('TW.txt', 'w', encoding='utf-8') as TW:
-    TW.write('\n台湾频道,#genre#\n')
-    for line in file:
-        if re.search(pattern, line):  # 如果行中有任意关键字
-            TW.write(line)  # 将该行写入输出文件
+        # 标记任务完成
+        task_queue.task_done()
 
-# 读取要合并的香港频道和台湾频道文件
-file_contents = []
-file_paths = ["HK.txt", "TW.txt"]  # 替换为实际的文件路径列表
-for file_path in file_paths:
-    with open(file_path, 'r', encoding="utf-8") as file:
-        content = file.read()
-        file_contents.append(content)
-# 生成合并后的文件
-with open("GAT.txt", "w", encoding="utf-8") as output:
-    output.write('\n'.join(file_contents))
+
+# 创建多个工作线程
+num_threads = 20
+for _ in range(num_threads):
+    t = threading.Thread(target=worker, daemon=True)
+    t.start()
+
+# 添加下载任务到队列
+for result in results:
+    task_queue.put(result)
+
+# 等待所有任务完成
+task_queue.join()
+
 
 def channel_key(channel_name):
     match = re.search(r'\d+', channel_name)
@@ -206,20 +258,22 @@ def channel_key(channel_name):
         return float('inf')  # 返回一个无穷大的数字作为关键字
 
 
-for result in results:
-    channel_name, channel_url = result.split(',', 1)
+for resulta in resultsx:
+    channel_name, channel_url = resulta
     resultx = channel_name, channel_url
     resultxs.append(resultx)
+
 # 对频道进行排序
 resultxs.sort(key=lambda x: channel_key(x[0]))
+# now_today = datetime.date.today()
 
-result_counter = 30  # 每个频道需要的个数
+result_counter = 80  # 每个频道需要的个数
 
 with open("iptv_list.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('央视频道,#genre#\n')
-    for resultx in resultxs:
-        channel_name, channel_url = resultx
+    for result in resultxs:
+        channel_name, channel_url = result
         if 'CCTV' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
@@ -231,10 +285,10 @@ with open("iptv_list.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
-    file.write('卫视频道,#genre#\n')
-    for resultx in resultxs:
-        channel_name, channel_url = resultx
-        if '卫视' in channel_name:
+    file.write('\n卫视频道,#genre#\n')
+    for result in resultxs:
+        channel_name, channel_url = result
+        if '卫视' in channel_name or '凤凰' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
@@ -244,10 +298,10 @@ with open("iptv_list.txt", 'w', encoding='utf-8') as file:
             else:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
-        channel_counters = {}
-    file.write('湖南频道,#genre#\n')
-    for resultx in resultxs:
-        channel_name, channel_url = resultx
+    channel_counters = {}
+    file.write('\n湖南频道,#genre#\n')
+    for result in resultxs:
+        channel_name, channel_url = result
         if '湖南' in channel_name or '长沙' in channel_name or '金鹰' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
@@ -259,10 +313,11 @@ with open("iptv_list.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
     channel_counters = {}
-    file.write('其他频道,#genre#\n')
-    for resultx in resultxs:
-        channel_name, channel_url = resultx
-        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name and '湖南' not in channel_name and '长沙' not in channel_name and '金鹰' not in channel_name:
+    file.write('\n其他频道,#genre#\n')
+    for result in resultxs:
+        channel_name, channel_url = result
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name and '湖南' not in \
+            channel_name and '长沙' not in channel_name and '金鹰' not in channel_name and '凤凰' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
@@ -272,25 +327,28 @@ with open("iptv_list.txt", 'w', encoding='utf-8') as file:
             else:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
-                
-# 合并所有的txt文件
-file_contents = []
-file_paths = ["iptv_list.txt", "GAT.txt", "zdy.txt"]  # 替换为实际的文件路径列表
-for file_path in file_paths:
-    with open(file_path, 'r', encoding="utf-8") as file:
-        content = file.read()
-        file_contents.append(content)
 
-# 写入合并后的txt文件
-with open("iptv_list.txt", "w", encoding="utf-8") as output:
-    output.write('\n'.join(file_contents))
-# 写入更新日期时间
-    now = datetime.now()
-    output.write(f"更新时间,#genre#\n")
-    output.write(f"{now.strftime("%Y-%m-%d")},url\n")
-    output.write(f"{now.strftime("%H:%M:%S")},url\n")
+    # 合并所有的txt文件
+    file_contents = []
+    file_paths = ["iptv_list.txt", "GAT.txt", "zdy.txt"]  # 替换为实际的文件路径列表
+    for file_path in file_paths:
+        with open(file_path, 'r', encoding="utf-8") as file:
+            content = file.read()
+            file_contents.append(content)
 
-os.remove("HK.txt")
-os.remove("TW.txt")
-os.remove("GAT.txt")
+    # 写入合并后的txt文件
+    with open("iptv_list.txt", "w", encoding="utf-8") as output:
+        output.write('\n'.join(file_contents))
+        # 写入更新日期时间
+        # file.write(f"{now_today}更新,#genre#\n")
+        now = datetime.now()
+        output.write(f"更新时间,#genre#\n")
+        output.write(f"{now.strftime("%Y-%m-%d")},url\n")
+        output.write(f"{now.strftime("%H:%M:%S")},url\n")
+
+    os.remove("DIYP-v4.txt")
+    os.remove("HK.txt")
+    os.remove("TW.txt")
+    os.remove("GAT.txt")
+
 print(f"电视频道成功写入iptv_list.txt")
