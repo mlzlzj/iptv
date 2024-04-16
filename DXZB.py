e3cb2c05bd0418543d3a076af3747939f307dedb
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-04-16 13:50:50 +0800

    Update DXZB.py

--- DXZB.py
+++ DXZB.py
@@ -341,6 +341,7 @@ for file_path in file_paths:
     # 写入合并后的txt文件
 with open("iptv_list.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
+            
 # 写入更新日期时间
     # file.write(f"{now_today}更新,#genre#\n")
     now = datetime.now()
@@ -354,4 +355,36 @@ os.remove("HK.txt")
 os.remove("TW.txt")
 os.remove("GAT.txt")
 
-print(f"电视频道成功写入iptv_dx.txt")
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U\n')
+
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            # if "," in line: # 防止文件里面缺失“,”号报错
+            if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
+
+
+# 将txt文件转换为m3u文件
+txt_to_m3u('iptv_list.txt', 'iptv_list.m3u')
+
+
+print(f"电视频道成功写入iptv_dx.txt和iptv_list.m3u")
