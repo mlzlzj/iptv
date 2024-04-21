1b90e9bb2df8e1b21ff17cafbd8d552ba76e2634
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-04-21 09:36:54 +0800

    Update DXZB.py

--- DXZB.py
+++ DXZB.py
@@ -355,36 +355,35 @@ os.remove("HK.txt")
 os.remove("TW.txt")
 os.remove("GAT.txt")
 
-# def txt_to_m3u(input_file, output_file):
-#     # 读取txt文件内容
-#     with open(input_file, 'r', encoding='utf-8') as f:
-#         lines = f.readlines()
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
 
-#     # 打开m3u文件并写入内容
-#     with open(output_file, 'w', encoding='utf-8') as f:
-#         f.write('#EXTM3U\n')
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U\n')
 
-#         # 初始化genre变量
-#         genre = ''
+        # 初始化genre变量
+        genre = ''
 
-#         # 遍历txt文件内容
-#         for line in lines:
-#             line = line.strip()
-#             # if "," in line: # 防止文件里面缺失“,”号报错
-#             if line:
-#                 # 检查是否是genre行
-#                 channel_name, channel_url = line.split(',', 1)
-#                 if channel_url == '#genre#':
-#                     genre = channel_name
-#                     print(genre)
-#                 else:
-#                     # 将频道信息写入m3u文件
-#                     f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
-#                     f.write(f'{channel_url}\n')
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            # if "," in line: # 防止文件里面缺失“,”号报错
+            if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
 
 
-# # 将txt文件转换为m3u文件
-# txt_to_m3u('iptv_list.txt', 'iptv_list.m3u')
-
+# 将txt文件转换为m3u文件
+txt_to_m3u('iptv_list.txt', 'iptv_list.m3u')
 
 print(f"电视频道成功写入iptv_dx.txt和iptv_list.m3u")
