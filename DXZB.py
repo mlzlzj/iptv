48eab041421a9caa5807bc1c25f061d8e33f7e99
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-11-17 22:16:35 +0800

    Update DXZB.py

--- DXZB.py
+++ DXZB.py
@@ -73,7 +73,7 @@ def replace_ip_in_channels(ip, channels):
 unique_ip_ports = set()
 
 # 读取配置文件
-config_path = 'config_dx.txt'
+config_path = 'config.txt'
 configs = read_config(config_path)
 
 # 使用集合去除配置文件内重复的 IP 地址及端口
