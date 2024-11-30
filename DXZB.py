941c99b397bcd61a6d33c68a33888f34cb1782c5
mlzlzj <161783521+mlzlzj@users.noreply.github.com>
2024-11-30 18:02:20 +0800

    Update DXZB.py

--- DXZB.py
+++ DXZB.py
@@ -6,7 +6,37 @@ import threading
 from queue import Queue
 from threading import Thread
 from datetime import datetime
+from datetime import timedelta
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+def should_run():
+    time_file_path = 'zubo/update_time.txt'
+
+    # 如果时间文件不存在，说明需要执行
+    if not os.path.exists(time_file_path):
+        return True
+
+    # 读取时间文件的内容
+    with open(time_file_path, 'r') as file:
+        last_run_time_str = file.read().strip()
+        last_run_time = datetime.strptime(last_run_time_str, '%Y-%m-%d %H:%M:%S')
+
+    # 获取当前时间
+    current_time = datetime.now()
+
+    # 判断当前时间与上次执行时间的差异是否大于等于三天
+    if current_time - last_run_time >= timedelta(days=3):
+        return True
+
+    return False
+
+
+def update_run_time():
+    time_file_path = 'zubo/update_time.txt'
+    current_time = datetime.now()
+    with open(time_file_path, 'w') as file:
+        file.write(current_time.strftime('%Y-%m-%d %H:%M:%S'))
 
 
 def check_ip(ip, port):
@@ -29,8 +59,16 @@ def generate_ips(ip_part, option):
         return [f"{a}.{b}.{c}.{d}" for c in range(0, 256) for d in range(0, 256)]
 
 
-def save_to_file(filename, valid_ips):
-    with open(filename, 'w') as f:
+def save_to_file(foldername, filename, valid_ips):
+    if not os.path.exists(foldername):
+        os.makedirs(foldername)
+
+    current_date = datetime.now().strftime("%Y%m%d")
+    filename = f"{current_date}_{filename}"
+
+    filepath = os.path.join(foldername, filename)
+
+    with open(filepath, 'w') as f:
         for ip in valid_ips:
             f.write(f"{ip}\n")
 
@@ -73,71 +111,73 @@ def replace_ip_in_channels(ip, channels):
 unique_ip_ports = set()
 
 # 读取配置文件
-config_path = 'config.txt'
-configs = read_config(config_path)
+if should_run():
+    update_run_time()
+    config_path = 'config.txt'
+    configs = read_config(config_path)
 
-# 使用集合去除配置文件内重复的 IP 地址及端口
-unique_configs = []
-for ip_part, port, option in configs:
-    ip_port = f"{ip_part}:{port}"
-    if ip_port not in unique_ip_ports:
-        unique_ip_ports.add(ip_port)
-        unique_configs.append((ip_part, port, option))
+    # 使用集合去除配置文件内重复的 IP 地址及端口
+    unique_configs = []
+    for ip_part, port, option in configs:
+        ip_port = f"{ip_part}:{port}"
+        if ip_port not in unique_ip_ports:
+            unique_ip_ports.add(ip_port)
+            unique_configs.append((ip_part, port, option))
 
-# 执行 IP 扫描
-all_valid_ips = []
-for ip_part, port, option in unique_configs:
-    print(f"开始扫描地址: {ip_part}, 端口: {port}, 类型: {option} （类型为0扫描D段，类型为1扫描C,D段）")
-    ips_to_check = generate_ips(ip_part, option)
+    # 执行 IP 扫描
+    all_valid_ips = []
+    for ip_part, port, option in unique_configs:
+        print(f"开始扫描 地址: {ip_part}, 端口: {port}, 类型: {option} （默认类型为0扫描D段，类型为1时扫描C,D段）")
+        ips_to_check = generate_ips(ip_part, option)
 
-    valid_ips = []
-    total_ips = len(ips_to_check)
-    checked_count = [0]
-
-
-    def update_status(checked_count):
-        while checked_count[0] < total_ips:
-            print(f"扫描数量: {checked_count[0]}, 有效数量: {len(valid_ips)}")
-            time.sleep(10)
+        valid_ips = []
+        total_ips = len(ips_to_check)
+        checked_count = [0]
 
 
-    # 启动状态更新线程
-    status_thread = threading.Thread(target=update_status, args=(checked_count,))
-    status_thread.start()
+        def update_status(checked_count):
+            while checked_count[0] < total_ips:
+                print(f"扫描数量: {checked_count[0]}, 有效数量: {len(valid_ips)}")
+                time.sleep(10)
 
-    max_workers = 5 if option == 0 else 200  # 扫描IP线程数量
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        future_to_ip = {executor.submit(check_ip, ip, port): ip for ip in ips_to_check}
-        for future in as_completed(future_to_ip):
-            ip = future_to_ip[future]
-            result = future.result()
-            if result is not None:
-                valid_ips.append(result)
-            checked_count[0] += 1
 
-    # 等待状态线程结束
-    status_thread.join()
-    all_valid_ips.extend(valid_ips)
+        # 启动状态更新线程
+        status_thread = threading.Thread(target=update_status, args=(checked_count,))
+        status_thread.start()
 
-save_to_file('ip.txt', all_valid_ips)
+        max_workers = 10 if option == 0 else 100  # 扫描IP线程数量
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            future_to_ip = {executor.submit(check_ip, ip, port): ip for ip in ips_to_check}
+            for future in as_completed(future_to_ip):
+                ip = future_to_ip[future]
+                result = future.result()
+                if result is not None:
+                    valid_ips.append(result)
+                checked_count[0] += 1
 
-for ip in all_valid_ips:
-    print(ip)
+        # 等待状态线程结束
+        status_thread.join()
+        all_valid_ips.extend(valid_ips)
 
-# 替换组播ID并写入文件
-# 读取湖南_电信.txt文件中的频道列表
-with open('湖南_电信.txt', 'r', encoding='utf-8') as f:
-    channels = f.readlines()
+    save_to_file('ip', 'ip.txt', all_valid_ips)
 
-# 将所有替换后的频道列表写入湖南_组播.txt文件中
-with open('湖南_组播.txt', 'w', encoding='utf-8') as f:
     for ip in all_valid_ips:
-        replaced_channels = replace_ip_in_channels(ip, channels)
-        for channel in replaced_channels:
-            f.write(f"{channel}")
-        f.write("\n")
+        print(ip)
 
-print(f"共扫描获取到有效IP {len(all_valid_ips)} 个，已全部匹配到湖南_组播.txt文件中。\n")
+    # 替换组播ID并写入文件
+    # 读取湖南_电信.txt文件中的频道列表
+    with open('zubo/湖南_电信.txt', 'r', encoding='utf-8') as f:
+        channels = f.readlines()
+
+    # 将所有替换后的频道列表写入湖南_组播.txt文件中
+    with open('zubo/湖南_组播.txt', 'w', encoding='utf-8') as f:
+        for ip in all_valid_ips:
+            replaced_channels = replace_ip_in_channels(ip, channels)
+            for channel in replaced_channels:
+                f.write(f"{channel}")
+            f.write("\n")
+
+    print(f"共扫描获取到有效IP {len(all_valid_ips)} 个，已全部匹配到湖南_组播.txt文件中。\n")
 
 # 开始对组播源频道列表进行下载速度检测
 # 定义一个全局队列，用于存储需要测速的频道信息
@@ -149,7 +189,7 @@ speed_results = []
 
 # 读取iptv_list.txt文件中的所有频道，并将它们添加到队列中
 def load_channels_to_speed_test():
-    with open('湖南_组播.txt', 'r', encoding='utf-8') as file:
+    with open('zubo/湖南_组播.txt', 'r', encoding='utf-8') as file:
         for line in file:
             channel_info = line.strip().split(',')
             if len(channel_info) >= 2:  # 假设至少有名称和URL
@@ -201,15 +241,15 @@ start_speed_test_threads(10)  # 测试下载速度线程数
 speed_results.sort(reverse=True)
 
 # 写入分类排序后的频道信息
-with open("speed.txt", 'w', encoding='utf-8') as file:
+with open("zubo/speed.txt", 'w', encoding='utf-8') as file:
     for result in speed_results:
         download_rate, channel_name, channel_url = result
-        # if download_rate >= 0.01:  # 只写入下载速度大于或等于 0.01 MB/s 的频道
-        file.write(f"{channel_name},{channel_url},{download_rate}\n")
+        if download_rate >= 0.01:  # 只写入下载速度大于或等于 0.01 MB/s 的频道
+            file.write(f"{channel_name},{channel_url},{download_rate}\n")
 
 # 对经过下载速度检测后的所有组播频道列表进行分组排序
 # 从测速后的文件中读取频道列表
-with open('speed.txt', 'r', encoding='utf-8') as file:
+with open('zubo/speed.txt', 'r', encoding='utf-8') as file:
     channels = []
     for line in file:
         line = line.strip()
@@ -237,8 +277,8 @@ def group_and_sort_channels(channels):
             groups['央视频道,#genre#'].append((name, url, speed))
         elif '卫视' in name or '凤凰' in name or '翡翠' in name or 'CHC' in name:
             groups['卫视频道,#genre#'].append((name, url, speed))
-        elif ('湖南' in name or '金鹰' in name or '长沙' in name or '娄底' in name or '岳阳' in name or '常德' in name
-              or '张家界' in name or '怀化' in name or '新化' in name or '株洲' in name or '桂东' in name or '武冈' in name
+        elif ('湖南' in name or '金鹰' in name or '长沙' in name or '娄底' in name or '岳阳' in name or '张家界' in name
+              or '常德' in name or '怀化' in name or '新化' in name or '株洲' in name or '桂东' in name or '武冈' in name
               or '永州' in name or '津市' in name or '浏阳' in name or '湘潭' in name or '湘西' in name or '溆浦' in name
               or '益阳' in name or '衡阳' in name or '道县' in name or '邵阳' in name or '郴州' in name or '双峰' in name
               or '东安' in name or '中方' in name or '会同' in name or '双牌' in name or '城步' in name or '宁乡' in name
@@ -251,10 +291,7 @@ def group_and_sort_channels(channels):
 
         # 对每组进行排序
         for group in groups.values():
-            group.sort(key=lambda x: (
-                natural_key(x[0]),  # 名称自然排序
-                -float(x[2]) if x[2] is not None else float('-inf')  # 速度从高到低排序
-            ))
+            group.sort(key=lambda x: (natural_key(x[0]), -float(x[2]) if x[2] is not None else float('-inf')))
 
     # 筛选相同名称的频道，只保存9个
     filtered_groups = {}
@@ -270,7 +307,7 @@ def group_and_sort_channels(channels):
             if name not in seen_names:
                 seen_names[name] = 0
 
-            if seen_names[name] < 18:
+            if seen_names[name] < 15:
                 filtered_list.append(channel)
                 seen_names[name] += 1
             else:
@@ -296,8 +333,6 @@ def group_and_sort_channels(channels):
             file.write("\n")
             print()  # 打印空行分隔组
 
-    print(f"\n经过测速分类排序后的频道列表数量为: {total_channels} 个，已全部写入iptv_list.txt文件中。")
-
     # # 保存频道数量超过10个的频道列表到新文件
     # with open('Filtered_iptv.txt', 'w', encoding='utf-8') as file:
     #     for group_name, channel_list in overflow_groups.items():
@@ -306,19 +341,20 @@ def group_and_sort_channels(channels):
     #             for name, url, speed in channel_list:
     #                 file.write(f"{name},{url}\n")
     #             file.write("\n")  # 打印空行分隔组
+    print(f"\n经过测速分类排序后的频道列表数量为: {total_channels} 个，已全部写入iptv_list.txt文件中。")
     return groups
 
 
 # 对频道列表进行分组和排序
 grouped_channels = group_and_sort_channels(channels)
-os.remove("湖南_组播.txt")
-os.remove("speed.txt")
+# os.remove("湖南_组播.txt")
+# os.remove("speed.txt")
 # os.remove("ip.txt")
 
 #  获取远程直播源文件
-url = "http://aktv.top/live.txt"
-r = requests.get(url)
-open('AKTV.txt', 'wb').write(r.content)
+# url = "http://aktv.top/live.txt"
+# r = requests.get(url)
+# open('AKTV.txt', 'wb').write(r.content)
 
 # 合并所有的txt文件
 file_contents = []
